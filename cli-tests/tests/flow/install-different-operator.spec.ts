b4e5dd41d184f6a10212068e67fd1e0cc15da90b
Manish Chawla <mchawla16@yahoo.com>
2024-02-29 19:16:37 +0530

    Modified test

--- cli-tests/tests/flow/install-different-operator.spec.ts
+++ cli-tests/tests/flow/install-different-operator.spec.ts
@@ -71,7 +71,7 @@ test.describe('Everest CLI install', async () => {
 
     await test.step('run everest install command', async () => {
       //Install the mysql operator in mysql namespace
-      const out = await cli.everestExecSkipWizard(
+      let out = await cli.everestExecSkipWizard(
         `install --operator.mongodb=false --operator.postgresql=false --operator.xtradb-cluster=true --namespaces=mysql`,
       );
 
